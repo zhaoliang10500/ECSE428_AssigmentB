f174e245890f443fefca7fe2d7b2efc379cadb8b
Liang Zhao <zhaoliang10500@gmail.com>
2021-11-08 21:43:50 -0500

    initialization

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use( express.json() )
+
+app.listen(
+    PORT, 
+    () => console.log(`it's alive on http://localhost:${PORT}`)
+)
+
+app.get('/smile', (req, res) => {
+    res.status(200).send({
+        smile: '^ - ^',
+        size: 'large'
+    })
+});
+
+app.post('/smile/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!'})
+    }
+
+    res.send({
+        smile: `^ - ^ with your ${logo} and ID of ${id}`,
+    });
+});
